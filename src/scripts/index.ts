cfc4b1e23ffaedae39b30d5e28b4623b3e9c39ba
Bertrand <root@localhost>
2023-08-20 14:08:36 +0200

    dev: first commit

---
+++ src/scripts/index.ts
@@ -0,0 +1,5 @@
+import "../styles/index.css";
+
+const div_title = document.querySelector("#div_title");
+
+div_title.innerHTML = "Bonjour à tous !!!";
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+
+module.exports = {
+  entry: { app: ["./src/styles/index.css", "./src/scripts/index.ts"] },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: "src/index.html",
+        title: "simple leaflet project"
+      }),
+    new MiniCssExtractPlugin({ filename: "[name].css" })
+  ],  
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
